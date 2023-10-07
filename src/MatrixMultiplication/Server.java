3b4429797e843c66d91ba37bb427a2dcc18ec5a1
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-10-08 01:47:24 +0300

    remove unused imports

--- src/MatrixMultiplication/Server.java
+++ src/MatrixMultiplication/Server.java
@@ -2,7 +2,6 @@ package MatrixMultiplication;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
